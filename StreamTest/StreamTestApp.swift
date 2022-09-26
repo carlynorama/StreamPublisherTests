c9200a635883178f744dacdb34bd82cf7b97702c
carlynorama <carlynorama@gmail.com>
2022-09-26 15:09:16 -0700

    Initial Commit

---
+++ StreamTest/StreamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTestApp.swift
+//  StreamTest
+//
+//  Created by Labtanza on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct StreamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
